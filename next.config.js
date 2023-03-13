de1bf6bf2196e23a71c7b03b926951488aec84d2
ju <julienbmartel@gmail.com>
2023-03-13 01:40:25 -0500

    added PH embed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.producthunt.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,6 +1,22 @@
+import Image from "next/image";
+
 export function Footer() {
   return (
     <footer className="grid place-items-center py-36">
+      <div>
+        <a
+          href="https://www.producthunt.com/posts/auto-archillect-wallpapers?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-auto&#0045;archillect&#0045;wallpapers"
+          target="_blank"
+        >
+          <Image
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=383473&theme=light"
+            alt="Auto&#0032;Archillect&#0032;Wallpapers - Sync&#0032;your&#0032;lock&#0032;screen&#0032;with&#0032;Archillect&#0039;s&#0032;most&#0032;recent&#0032;image | Product Hunt"
+            className="mb-6 h-[54px] w-[250px]"
+            width="250"
+            height="54"
+          />
+        </a>
+      </div>
       <div className="mb-1.5 flex space-x-3">
         <a
           className="font-semibold"
